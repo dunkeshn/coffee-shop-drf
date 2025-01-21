8a70853f7eb6b2eb41cfe04b4aaa3ff8a3ebdbbc
Ivan <130477079+dunkeshn@users.noreply.github.com>
2025-01-21 13:26:38 +0500

    feat: added profile

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
+
+from users.models.profile import Profile
 from users.models.users import User
+
+
+class ProfileAdmin(admin.StackedInline):
+    model = Profile
+    fields = ('telegram_id', )
+
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
@@ -28,30 +36,4 @@ class UserAdmin(UserAdmin):
     filter_horizontal = ('groups', 'user_permissions', )
     readonly_fields = ('last_login', )
 
-    # fieldsets = (
-    #     (None, {'fields': ('username', 'phone_number',
-    #                        'phone_number_is_verified',
-    #                        'email', 'email_is_verified',)}),
-    #     ('Личная информация',
-    #     {'fields': ('first_name', 'last_name',)}),
-    #     ('Permissions',
-    #         {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-    #     ('Important dates', {'fields': ('last_login',)}),
-    # )
-    # add_fieldset = (
-    #     (None, {
-    #         'classes': ('wide',),
-    #         'fields': ('email', 'phone_number', 'password1', 'password2',),
-    #     }),
-    # )
-    # list_display = (
-    #     'id', 'full_name', 'profile_link', 'email',
-    #     'email_is_verified', 'phone_number',
-    #     'phone_number_is_verified',
-    # )
-    # list_display_links = ('id', 'full_name', )
-    # list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', )
-    # search_fields = ('first_name', 'last_name', 'id', 'email', 'phone_number', )
-    # ordering = ('-id', )
-    # filter_horizontal = ('groups', 'user_permissions', )
-    # readonly_fields = ('last_login', )
+    inlines = (ProfileAdmin, )
---
+++ users/models/profile.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+# User = get_user_model()
+class Profile(models.Model):
+    user = models.OneToOneField('users.User',
+                                on_delete=models.CASCADE,
+                                related_name='profile',
+                                verbose_name='Пользователь',
+                                primary_key=True,
+                                )
+    telegram_id = models.CharField('Telegram', max_length=64, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Профиль пользователя'
+        verbose_name_plural = 'Профили пользователей'
+
+    def __str__(self):
+        return f'{self.user} {self.pk}'
