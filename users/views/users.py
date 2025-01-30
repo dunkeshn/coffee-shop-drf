9dd8f92a07f4fcfbb939667fe8754c4bd9c2569d
Ivan <130477079+dunkeshn@users.noreply.github.com>
2025-01-30 12:07:15 +0500

    feat: profile model added and added features in user serializers

--- users/views/users.py
+++ users/views/users.py
@@ -1,7 +1,12 @@
+import pdb
+
 from django.contrib.auth import get_user_model
 from drf_spectacular.utils import extend_schema_view, extend_schema
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.views import APIView
 
 from users.serializers.api.users import RegistrationSerializer
 from users.serializers.api import users as user_s
@@ -15,3 +20,42 @@ class RegistrationView(CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = user_s.RegistrationSerializer
+
+
+@extend_schema_view(
+    post=extend_schema(request=user_s.ChangePasswordSerializer, # Для APIView (?)!!!!!!!!
+                       summary='Смена пароля',
+                       tags=['Аунтефикация & Авторизация']),
+)
+class ChangePasswordView(APIView):
+    def post(self, request):
+        user = request.user
+        serializer = user_s.ChangePasswordSerializer(
+            instance=user, data=request.data
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=HTTP_204_NO_CONTENT)
+
+
+@extend_schema_view(
+    get=extend_schema(summary='Профиль пользователя',
+                       tags=['Пользователи']),
+    put=extend_schema(summary='Изменить профиль пользователя',
+                       tags=['Пользователи']),
+    patch=extend_schema(summary='Частично изменить профиль пользователя',
+                       tags=['Пользователи']),
+)
+class MeView(RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = user_s.MeSerializer
+    http_method_names = ('get', 'patch')
+
+
+    def get_serializer_class(self): # Переопределяем метод, чтобы в зависимости от метода выбирался нужный сериализатор
+        if self.request.method in ['PUT', 'PATCH']:
+            return user_s.MeUpdateSerializer
+        return user_s.MeSerializer
+
+    def get_object(self): # Необязательно
+        return self.request.user
